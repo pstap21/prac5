8832cce8b4103a3a3ffaad3781bdfa7c71a99aa8
pstap21 <a1629853@adelaide.edu.au>
2025-04-10 13:56:57 +0930

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -12,26 +12,3 @@ int Vehicle::getID() const {
 
 Vehicle::~Vehicle() {}
 
-Car::Car(int id) : Vehicle(id) {}
-
-int Car::getParkingDuration() const {
-    std::time_t now = std::time(nullptr);
-    int duration = static_cast<int>(difftime(now, timeOfEntry));
-    return static_cast<int>(duration * 0.9); // 10% reduction
-}
-
-Bus::Bus(int id) : Vehicle(id) {}
-
-int Bus::getParkingDuration() const {
-    std::time_t now = std::time(nullptr);
-    int duration = static_cast<int>(difftime(now, timeOfEntry));
-    return static_cast<int>(duration * 0.75); // 25% reduction
-}
-
-Motorbike::Motorbike(int id) : Vehicle(id) {}
-
-int Motorbike::getParkingDuration() const {
-    std::time_t now = std::time(nullptr);
-    int duration = static_cast<int>(difftime(now, timeOfEntry));
-    return static_cast<int>(duration * 0.85); // 15% reduction
-}
